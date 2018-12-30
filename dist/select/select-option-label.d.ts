edd3ea431ff1c51533f207579e680ed959e40cb3
Levy <levy@codeandmore.pl>
2018-12-30 22:59:58 +0100

    Commit

--- dist/select/select-option-label.d.ts
+++
@@ -1,6 +0,0 @@
-import { TemplateRef, ViewContainerRef } from "@angular/core";
-export declare class SelectLabel {
-    readonly templateRef: TemplateRef<any>;
-    readonly viewContainer: ViewContainerRef;
-    constructor(templateRef: TemplateRef<any>, viewContainer: ViewContainerRef);
-}
