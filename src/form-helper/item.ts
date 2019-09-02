864053b8613853f775771150b5a554ae77a263e7
Levy <levy@codeandmore.pl>
2019-09-02 19:31:38 +0200

    Commit

--- src/form-helper/item.ts
+++ src/form-helper/item.ts
@@ -2,6 +2,7 @@ import {Component} from "@angular/core";
 
 @Component({
     selector: "ionx-form-item",
+    styleUrls: ["item.scss"],
     template: `<ng-content select="ion-item"></ng-content><ng-content select="ionx-form-item-error"></ng-content><ng-content select="ionx-form-item-hint"></ng-content><ng-content></ng-content>`
 })
 export class FormItem {
