10a9d21fb35cb9be65ed2f20d2bcb170c83bb5be
Levy <levy@codeandmore.pl>
2019-09-02 19:06:16 +0200

    Commit

--- dist/lazy-image/lazy-image.d.ts
+++ dist/lazy-image/lazy-image.d.ts
@@ -1,4 +1,15 @@
 import { ElementRef, QueryList, Renderer } from "@angular/core";
+export declare class LazyImageContainer {
+    private element;
+    constructor(element: ElementRef<HTMLElement>);
+    private lazyLoad;
+    revalidate(): void;
+    children: QueryList<LazyImage>;
+    ngOnInit(): void;
+    ngAfterContentInit(): void;
+    ngOnDestroy(): void;
+    private initLazyLoad;
+}
 export declare class LazyImage {
     element: ElementRef;
     private renderer;
@@ -12,15 +23,4 @@ export declare class LazyImage {
     private reset;
     private revalidate;
     ngAfterContentInit(): void;
-}
-export declare class LazyImageContainer {
-    private element;
-    constructor(element: ElementRef<HTMLElement>);
-    private lazyLoad;
-    revalidate(): void;
-    children: QueryList<LazyImage>;
-    ngOnInit(): void;
-    ngAfterContentInit(): void;
-    ngOnDestroy(): void;
-    private initLazyLoad;
 }
