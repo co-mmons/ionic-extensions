ec96f6df1738d00901e8e1eb3d7c2ee7f0291c0f
Levy <levy@codeandmore.pl>
2019-11-09 10:46:32 +0100

    Commit

--- dist/dialog/index.d.ts
+++ dist/dialog/index.d.ts
@@ -3,5 +3,6 @@ export { DialogButton } from "./dialog-button";
 export { DialogController } from "./dialog-controller";
 export { dialogData } from "./dialog-data-symbol";
 export { dialogInstance } from "./dialog-instance-symbol";
+export { DialogMessageComponent } from "./dialog-message-component";
 export declare class DialogModule {
 }
--- src/dialog/index.ts
+++ src/dialog/index.ts
@@ -11,6 +11,7 @@ export {DialogButton} from "./dialog-button";
 export {DialogController} from "./dialog-controller";
 export {dialogData} from "./dialog-data-symbol";
 export {dialogInstance} from "./dialog-instance-symbol";
+export {DialogMessageComponent} from "./dialog-message-component";
 
 @NgModule({
     declarations: [Dialog],
